fe057fa82ca686d63f5c05bb64da8af1af55a5ac
alicjamarciniak <marciniak.al@gmail.com>
2023-11-06 15:21:46 +0100

    [feat] Adds components barrel file

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export * from "./buttons";
+export * from "./cards";
+export * from "./layout";
+
+export { default as Select } from "./select/Select";
+export { default as RadioGroup } from "./radio-group/RadioGroup";
+export { default as Pill } from "./pill/Pill";
+export { default as NoData } from "./noData/NoData";
+export { default as Affix } from "./affix/Affix";
