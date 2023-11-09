bfbe8f6419275dff71de99b7dd0980d55ce9d156
alicjamarciniak <marciniak.al@gmail.com>
2023-11-09 18:21:53 +0100

    [fix] changes default device to mobile

--- src/hooks/useDevices.ts
+++ src/hooks/useDevices.ts
@@ -1,10 +1,10 @@
 import { useMediaQuery } from "@mantine/hooks";
 
+// TODO: use better option to detect device before page load
 const useDevices = () => {
-  const isMobile =
-    useMediaQuery("(max-width: 768px)", false, {
-      getInitialValueInEffect: false,
-    }) ?? false;
+  const isMobile = useMediaQuery("(max-width: 768px)", true, {
+    getInitialValueInEffect: false,
+  });
 
   return { isMobile };
 };
